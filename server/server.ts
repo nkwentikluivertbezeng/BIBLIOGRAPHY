8452c08078b0423d766f5c13df493624f77bde4c
gills-lambv <gillslambiv@gmail.com>
2023-05-18 15:57:24 +0100

    feat(server): add endpoint to get all books

--- server/server.ts
+++ server/server.ts
@@ -3,12 +3,14 @@ import App from './app'
 import * as bodyParser from 'body-parser'
 import loggerMiddleware from './middleware/logger'
 import HomeController from './controllers/home.controller'
+import BookController from './controllers/book.controller'
 
 
 const app = new App({
     port: 3000,
     controllers: [
         new HomeController(),
+        new BookController()
     ],
     middleWares: [
         bodyParser.json(),
